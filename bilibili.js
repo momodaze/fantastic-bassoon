d763b0ee3a691f0b2ff6fca03578af72e78584c7
momodaze <wychen.1216@qq.com>
2026-02-14 17:14:14 +0800

    Add files via upload

--- bilibili.js
+++ bilibili.js
@@ -4,6 +4,7 @@
 //修改了函，新增位移
 //写了个函数
 function cardOut(trapper, card) {
+  console.log('cardOut被调用:', trapper, card)
   let timer
   trapper.addEventListener('mouseenter', () => {
     clearTimeout(timer)
@@ -12,7 +13,7 @@ function cardOut(trapper, card) {
   trapper.addEventListener('mouseleave', () => {
     timer = setTimeout(() => {
       card.classList.remove('move')
-    }, 300);
+    }, 200);
   })
   card.addEventListener('mouseenter', () => {
     clearTimeout(timer)
@@ -24,10 +25,50 @@ function cardOut(trapper, card) {
 
   })
 }
-
 cardOut(document.querySelector('.tubiao-more1'), document.querySelector('.gamecard'))
 cardOut(document.querySelector('.tubiao-more2'), document.querySelector('.mangacard'))
 cardOut(document.querySelector('.tubiao-more3'), document.querySelector('.download'))
+//
+const user = document.querySelector('.user');
+const userIcon = document.querySelector('.user-icon');
+//单独写一个这个user，这个需要判断鼠标悬停的状态决定move的移除
+if (user && userIcon) {
+  let timer2
+
+  userIcon.addEventListener('mouseenter', () => {
+    clearTimeout(timer2);
+    userIcon.classList.add('move');
+    user.classList.add('move');
+  })
+
+  userIcon.addEventListener('mouseleave', () => {
+    timer2 = setTimeout(() => {
+      if (!user.matches(':hover')) {
+        userIcon.classList.remove('move');
+        user.classList.remove('move')
+      }
+    }, 200)
+  })
+
+  user.addEventListener('mouseenter', () => {
+    clearTimeout(timer2);
+    userIcon.classList.add('move')
+    user.classList.add('move')
+  })
+
+  user.addEventListener('mouseleave', () => {
+    if (!userIcon.matches(':hover')) {
+      userIcon.classList.remove('move')
+      user.classList.remove('move')
+    }
+  })
+}
+cardOut(document.querySelector('.big'), document.querySelector('.vip'))
+cardOut(document.querySelector('.messages'), document.querySelector('.mes'))
+cardOut(document.querySelector('.newPos'), document.querySelector('.newpos'))
+cardOut(document.querySelector('.colector'), document.querySelector('.colect'));
+cardOut(document.querySelector('.hist'), document.querySelector('.history'));
+cardOut(document.querySelector('.submit'), document.querySelector('.putup'));
 
 //页面滚动出现小组件
 
@@ -100,7 +141,7 @@ navv.addEventListener('mouseenter', () => {
   showBox.style.transform = 'translateY(90px)';
   showBox.style.opacity = '1';
   showBox.style.pointerEvents = 'auto';
-});
+})
 
 navv.addEventListener('mouseleave', () => {
   navv.style.overflow = 'hidden';
@@ -118,4 +159,36 @@ rotate.addEventListener('mouseenter', () => {
 })
 rotate.addEventListener('mouseleave', () => {
   arrow.style.transform = 'rotate(0deg)'
+})
+//很多很多的分类栏。。。我要写吐了怎么这么多诡异的分类和导航
+const touch = document.querySelector('.touch')
+const moreCato = document.querySelector('.more-cato')
+let timerr
+touch.addEventListener('mouseenter', () => {
+  if (document.documentElement.scrollTop > 64) {
+    clearTimeout(timerr)
+    moreCato.classList.add('out')
+  }
+})
+touch.addEventListener('mouseleave', () => {
+  if (document.documentElement.scrollTop > 64) {
+    timerr = setTimeout(() => {
+      moreCato.classList.remove('out')
+    }, 500)
+  }
+})
+//再单独写一个鼠标移到分类上
+let aTime
+moreCato.addEventListener('mouseenter', () => {
+  if (document.documentElement.scrollTop > 64) {
+    clearTimeout(aTime)
+    moreCato.classList.add('out')
+  }
+})
+moreCato.addEventListener('mouseleave', () => {
+  if (document.documentElement.scrollTop > 64) {
+    aTime = setTimeout(() => {
+      moreCato.classList.remove('out')
+    }, 500)
+  }
 })
